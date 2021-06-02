e66abeca233fc9dc9455a1091df14c6006cca07a
svenWacker <s.u.wacker@outlook.de>
2021-06-02 15:41:41 +0200

    scss folders

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import Header from "./components/Header";
 import Gallery from "./components/Gallery";
 import Data from "./data.json";
 import Footer from "./components/Footer";
-import ReactDOM from "react-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+
+// import ReactDOM from "react-dom";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+// const element = <FontAwesomeIcon icon={faCoffee} />;
 
 function App() {
   const [posts, setPosts] = useState(Data);
@@ -21,5 +23,4 @@ function App() {
     </React.Fragment>
   );
 }
-
 export default App;
