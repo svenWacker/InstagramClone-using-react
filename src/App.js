a4571d3a168dffdbec5c5e474f9c8294c25b6f59
svenWacker <s.u.wacker@outlook.de>
2021-06-02 11:27:06 +0200

    Updating file and folder setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Gallery from "./components/Gallery";
+import Data from "./data.json";
+
 function App() {
+  // const [posts, setPosts] = useState(Data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Instagram Clone App</h1>
-      </header>
-    </div>
+    <React.Fragment>
+      <h1>Instagram Clone App</h1>
+      {
+        // <Gallery posts={posts} />
+      }
+      <Gallery Data={Data} />
+    </React.Fragment>
   );
 }
 
