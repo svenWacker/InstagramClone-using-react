5902ec01e52975235f12fe95dd733af4982aaa30
svenWacker <s.u.wacker@outlook.de>
2021-07-06 13:23:43 +0200

    working on gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,15 +3,17 @@ import { faHeart, faComment } from "@fortawesome/free-solid-svg-icons";
 
 const Gallery = ({ Data }) => {
   const galleryData = Data.map((obj) => (
-    <li key={obj.id}>
+    <li key={obj.id} className="#images">
       <img src={obj.img} alt={obj.title} className="images" />
       <div className="image-overlay">
         <div className="image-title">
           <FontAwesomeIcon icon={faHeart} />
+          &nbsp;
           {obj.likes}
         </div>
         <div className="image-description">
           <FontAwesomeIcon icon={faComment} />
+          &nbsp;
           {obj.comments}
         </div>
       </div>
